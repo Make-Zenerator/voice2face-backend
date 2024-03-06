ef73dcf7d1d6c84a35d606f9bfb008aab5011d29
soyoonjeong <76814748+soyoonjeong@users.noreply.github.com>
2024-03-06 18:57:23 +0900

    Feat : celery task 부분 일단 주석 처리

--- backend/simple_worker/tasks.py
+++ backend/simple_worker/tasks.py
@@ -17,37 +17,38 @@ def run_mz(request_id, result_id, age, gender, file_url):
     logger.info('Got Request - Starting work')
     time.sleep(4)
     logger.info(age, gender, file_url)
-    target_server_url = ''
-    params = {'age' : age, 'gender' : gender, 'voice_url': file_url}
-    try: # ML server로 요청 보내기 
-        response = requests.get(target_server_url, params = params)
-        # response은 condition_image_url, condition_gif_url, voice_image_url, voice_gif_url로 구성 
-        if response.status_code == 200: # 성공 시 
-            response.request_id = request_id
-            response.result_id = result_id
+    # target_server_url = ''
+    # params = {'age' : age, 'gender' : gender, 'voice_url': file_url}
+    # try: # ML server로 요청 보내기 
+    #     response = requests.get(target_server_url, params = params)
+    #     # response은 condition_image_url, condition_gif_url, voice_image_url, voice_gif_url로 구성 
+    #     if response.status_code == 200: # 성공 시 
+    #         response.request_id = request_id
+    #         response.result_id = result_id
 
-            # Update status
-            status_to_change = 'SUCCESS'
-            result, message = module.db_module.update_mz_request_status(request_id, status_to_change)
-            logger.info(message)
+    #         # Update status
+    #         status_to_change = 'SUCCESS'
+    #         result, message = module.db_module.update_mz_request_status(request_id, status_to_change)
+    #         logger.info(message)
 
-            # Update result 
-            result_to_change = {
-                'condition_image_url' : response.condition_image_url,
-                'condition_gif_url' : response.condition_gif_url,
-                'voice_image_url' : response.voice_image_url,
-                'voice_gif_url' : response.voice_image_url
-            }
-            result, message = module.db_module.update_mz_result_image_gif(request_id, result_to_change)
-            logger.info(message)
+    #         # Update result 
+    #         result_to_change = {
+    #             'condition_image_url' : response.condition_image_url,
+    #             'condition_gif_url' : response.condition_gif_url,
+    #             'voice_image_url' : response.voice_image_url,
+    #             'voice_gif_url' : response.voice_image_url
+    #         }
+    #         result, message = module.db_module.update_mz_result_image_gif(request_id, result_to_change)
+    #         logger.info(message)
 
-        else: # 실패 시 
-            # Update status
-            status_to_change = 'FAILED'
-            result, message = module.db_module.update_mz_request_status(request_id, status_to_change)
+    #     else: # 실패 시 
+    #         # Update status
+    #         status_to_change = 'FAILED'
+    #         result, message = module.db_module.update_mz_request_status(request_id, status_to_change)
 
-    except requests.RequestException as e:
-        return f'Request failed with exception: {str(e)}'
+    # except requests.RequestException as e:
+    #     return f'Request failed with exception: {str(e)}'
 
     logger.info('Work Finished ')
-    return response.status_code
+    # return response.status_code
+    return 200
