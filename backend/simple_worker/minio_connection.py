b5aab19cc378e2fbd05305d7d7a4199955d323f4
soyoonjeong <76814748+soyoonjeong@users.noreply.github.com>
2024-03-13 05:10:48 +0900

    Fix : typo

--- backend/simple_worker/minio_connection.py
+++ backend/simple_worker/minio_connection.py
@@ -28,7 +28,7 @@ def minio_list_object(storage, age, gender):
     '''
     try:
         prefix = "output_condition"
-        contents_list = list(torage.list_objects(BUCKET_NAME, prefix)['Contents'])
+        contents_list = list(storage.list_objects(BUCKET_NAME, prefix)['Contents'])
         file_list = [content['Key'] for content in contents_list]
         condition_file_list = []
         for file in file_list:
