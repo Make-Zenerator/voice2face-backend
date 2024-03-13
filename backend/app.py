4d8d11eda6ebb872c00a6bd6a943528bcc0a5199
internationalwe <tlsghwns1122@gmail.com>
2024-03-13 02:50:35 +0000

    Feat: http to https

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 from flask_cors import CORS
 from flask_restx import Api
 from db.db_connection import db_connection, db
 # from prometheus_flask_exporter import PrometheusMetrics
 from apis_v1 import User, MzRequest
+import ssl
 
 app = Flask(__name__)
 # app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 # 용량제한
@@ -32,6 +33,13 @@ api.add_namespace(User.Users, '/users')
 api.add_namespace(User.Auth, '/auth')
 api.add_namespace(MzRequest.MzRequest, '/mz-request')
 
+@app.before_request
+def before_request():
+    if request.url.startswith('http://'):
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
+
 # @app.route('/users', methods=['OPTIONS'])
 # @app.route('/auth', methods=['OPTIONS'])
 # @app.route('/mz-request', methods=['OPTIONS'])
@@ -44,4 +52,6 @@ api.add_namespace(MzRequest.MzRequest, '/mz-request')
     #return response
 
 if __name__ == "__main__":
-    app.run(port=5050, debug=True)
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+    ssl_context.load_cert_chain(certfile='newcert.pem', keyfile='newkey.pem')
+    app.run(port=5050, debug=True,ssl_context=ssl_context)
