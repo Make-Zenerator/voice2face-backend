4a65795078de8dc78aa88c9be88ef7edc736d8cd
jouhy <ethda11@kookmin.ac.kr>
2024-03-18 01:14:10 +0900

    Feat : target url 지정

--- backend/simple_worker/tasks.py
+++ backend/simple_worker/tasks.py
@@ -19,7 +19,7 @@ def run_mz(request_id, result_id, age, gender, file_url):
     logger.info('Got Request - Starting work')
     time.sleep(4)
 
-    target_server_url = ''
+    target_server_url = 'https://175.106.97.56:3002/makevideo'
     params = {'age' : age, 'gender' : gender, 'voice_url': file_url, 'request_id' : request_id, 'result_id' : result_id}
     logger.info(params)
 
@@ -31,47 +31,37 @@ def run_mz(request_id, result_id, age, gender, file_url):
             condition_gif_url = message['gif']
             logger.info(condition_image_url, condition_gif_url)
 
-            # Update status
-            status_to_change = 'Success'
-            db.update_mz_request_status(request_id, status_to_change)
         else:
-            condition_image_url = None
-            condition_gif_url = None
-
             # Update status
             status_to_change = 'Failed'
             db.update_mz_request_status(request_id, status_to_change)
+            return 400
 
-        voice_image_url = None
-        voice_gif_url = None
+        # voice output
+        response = requests.post(target_server_url, params = params)
+        logger.info('response : ', response)
 
-        # # voice output
-        # response = requests.get(target_server_url, params = params)
-        # logger.info('response : ', response)
+        if response.status_code == 200: # 성공 시 
+            voice_image_url = response.voice_image_url
+            voice_gif_url = response.voice_gif_url
 
-        # if response.status_code == 200: # 성공 시 
-        #     voice_image_url = response.voice_image_url
-        #     voice_gif_url = response.voice_gif_url
+        else: # 실패 시 
+            # Update status
+            status_to_change = 'Failed'
+            result, message = db.update_mz_request_status(request_id, status_to_change)
+            return 400
 
-        #     # Update status
-        #     status_to_change = 'Success'
-        #     result, message = module.db_module.update_mz_request_status(request_id, status_to_change)
-        #     logger.info(message)
-
-        # else: # 실패 시 
-        #     voice_image_url = None
-        #     voice_gif_url = None
-
-        #     # Update status
-        #     status_to_change = 'Failed'
-        #     result, message = module.db_module.update_mz_request_status(request_id, status_to_change)
+        # Update status
+        status_to_change = 'Success'
+        result, message = db.update_mz_request_status(request_id, status_to_change)
+        logger.info(message)
 
         # Update result 
         result_to_change = {
             'condition_image_url' : condition_image_url,
             'condition_gif_url' : condition_gif_url,
             'voice_image_url' : voice_image_url,
-            'voice_gif_url' : voice_image_url
+            'voice_gif_url' : voice_gif_url
         }
         print(result_to_change)
         db.update_mz_result_image_gif(request_id, result_to_change)
