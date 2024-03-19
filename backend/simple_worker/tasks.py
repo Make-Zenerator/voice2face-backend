2bf3f75f09f1bb2170aa96386b59c17bc0cc818e
soyoonjeong <76814748+soyoonjeong@users.noreply.github.com>
2024-03-20 04:57:42 +0900

    Feat : post 요청 받을 url 주소 변경

--- backend/simple_worker/tasks.py
+++ backend/simple_worker/tasks.py
@@ -20,7 +20,7 @@ def run_mz(request_id, result_id, age, gender, file_url):
     logger.info('Got Request - Starting work')
     time.sleep(4)
 
-    target_server_url = 'http://175.106.97.56:3002/makevideo'
+    target_server_url = 'https://sf2f.makezenerator.com/makevideo'
     data = {'age' : age, 
             'gender' : gender, 
             'voice_url': file_url, 
@@ -62,7 +62,7 @@ def run_mz(request_id, result_id, age, gender, file_url):
             logger.info(error)
             status_to_change = 'Failed'
             db.update_mz_request_status(request_id, status_to_change)
-        #     return 400
+            return 400
 
         # Update status
         status_to_change = 'Success'
