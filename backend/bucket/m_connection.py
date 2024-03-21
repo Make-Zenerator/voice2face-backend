e3250596f8c83c5912344f843096f2e90106f6f5
jouhy <ethda11@kookmin.ac.kr>
2024-03-21 17:22:12 +0900

    Feat : mp4로 음성 파일 받기

--- backend/bucket/m_connection.py
+++ backend/bucket/m_connection.py
@@ -30,7 +30,7 @@ def minio_put_object(storage, filename, data):
     '''
     try:
         #storage.fput_object(BUCKET_NAME, filename, data, content_type="audio/wav")
-        storage.fput_object(BUCKET_NAME, filename, data)
+        storage.fput_object(BUCKET_NAME, filename, data, content_type='audio/mp4')
         print(f"{filename} is successfully uploaded to bucket {BUCKET_NAME}.")
     except Exception as e:
         print(e)
